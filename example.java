3d16e49cb3b92a1ffddd4decc495039af1335642
Alberto Misail <misailalberto@gmail.com>
2020-08-30 15:27:42 -0700

    Translating example

--- example.java
+++ example.java
@@ -1,30 +1,34 @@
 package tck.java.time;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.junit.BeforeClass;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.runner.RunWith;
+import org.junit.Test;
 
 /**
  * Test LocalDate.
  */
-@Test
+@RunWith(DataProviderRunner.class)
 public class TCKLocalDate {
 
-    @DataProvider(name="sampleBadParse")
-    Object[][] provider_sampleBadParse() {
+    @DataProvider
+    public static Object[][] provider_sampleBadParse() {
         return new Object[][]{
                 {"2008/07/05"},
                 {"10000-01-01"},
         };
     }
 
-    @Test(dataProvider="sampleBadParse", expectedExceptions={DateTimeParseException.class})
+    @Test(expected=DateTimeParseException.class)
+    @UseDataProvider("provider_sampleBadParse")
     public void factory_parse_invalidText(String unparsable) {
         LocalDate.parse(unparsable);
     }
@@ -45,20 +49,20 @@ public class TCKLocalDate {
             for (int j = 0; j < localDates.length; j++) {
                 LocalDate b = localDates[j];
                 if (i < j) {
-                    assertTrue(a.compareTo(b) < 0, "Comparing" + a + " <=> " + b);
-                    assertEquals(a.isBefore(b), true, "Comparing" + a + " <=> " + b);
-                    assertEquals(a.isAfter(b), false, "Comparing" + a + " <=> " + b);
-                    assertEquals(a.equals(b), false, "Comparing" + a + " <=> " + b);
+                    assertTrue("Comparing" + a + " <=> " + b, a.compareTo(b) < 0);
+                    assertEquals("Comparing" + a + " <=> " + b, a.isBefore(b), true);
+                    assertEquals("Comparing" + a + " <=> " + b, a.isAfter(b), false);
+                    assertEquals("Comparing" + a + " <=> " + b, a.equals(b), false);
                 } else if (i > j) {
-                    assertTrue(a.compareTo(b) > 0, "Comparing" + a + " <=> " + b);
-                    assertEquals(a.isBefore(b), false, "Comparing" + a + " <=> " + b);
-                    assertEquals(a.isAfter(b), true, "Comparing" + a + " <=> " + b);
-                    assertEquals(a.equals(b), false, "Comparing" + a + " <=> " + b);
+                    assertTrue("Comparing" + a + " <=> " + b, a.compareTo(b) > 0);
+                    assertEquals("Comparing" + a + " <=> " + b, a.isBefore(b), false);
+                    assertEquals("Comparing" + a + " <=> " + b, a.isAfter(b), true);
+                    assertEquals("Comparing" + a + " <=> " + b, a.equals(b), false);
                 } else {
-                    assertEquals(a.compareTo(b), 0, "Comparing" + a + " <=> " + b);
-                    assertEquals(a.isBefore(b), false, "Comparing" + a + " <=> " + b);
-                    assertEquals(a.isAfter(b), false, "Comparing" + a + " <=> " + b);
-                    assertEquals(a.equals(b), true, "Comparing" + a + " <=> " + b);
+                    assertEquals("Comparing" + a + " <=> " + b, a.compareTo(b), 0);
+                    assertEquals("Comparing" + a + " <=> " + b, a.isBefore(b), false);
+                    assertEquals("Comparing" + a + " <=> " + b, a.isAfter(b), false);
+                    assertEquals("Comparing" + a + " <=> " + b, a.equals(b), true);
                 }
             }
         }
